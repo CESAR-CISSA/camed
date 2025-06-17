ed3fe539eaa38f9d9152af4b2b204adfcf686ed8
Pedro Liberal <phgui.learn@proton.me>
2025-06-17 16:37:59 -0300

    feat : add docker network creation in nmon script

--- cep.sh
+++ cep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # This variable is responsible to create a file with an specific RUN value.
-export RUN=1
+export RUN=33
 
-export SIDDHISDK_HOME=/home/user/Sidhi/siddhi-sdk-5.1.2 # Change your values acordatily
+export SIDDHISDK_HOME=/home/phgl/siddhi-sdk-5.1.2 # Change your values acordatily
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 sleep 10s; timeout 130s python3 app.py
--- start_nmon.sh
+++ start_nmon.sh
@@ -3,11 +3,13 @@
 nmon -f -s 1 -c 140
 sleep 140s;
 
-RUN=1  # This value has to be indentic to the one set into cep.sh file
+# Create a Docker network for MQTT
+docker network create mqtt-net
+
+RUN=33  # This value has to be indentic to the one set into cep.sh file
 arquivo_nmon=$(ls -t *.nmon | head -n 1)
 
 mv $arquivo_nmon $RUN.nmon
-
 
 awk -F',' '/^CPU_ALL/ {print $1","$2","$3","$4","$5}' $RUN.nmon > $RUN.cpu_overall.csv
 awk -F',' '/^MEM/ {print}' $RUN.nmon > $RUN.memory_data.csv
